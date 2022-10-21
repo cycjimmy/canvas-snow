da15722251f8642c806c49e8a58fe3a9e1df10aa
cycjimmy <cycjimmy@gmail.com>
2022-10-21 03:56:20 +0000

    Deploying to gh-pages from @ cycjimmy/canvas-snow@7e4d0a67940cd8919c69ad431a29a3a460319f5d 🚀

--- canvas-snow.umd.js
+++ canvas-snow.umd.js
@@ -9,7 +9,6 @@
       throw new TypeError("Cannot call a class as a function");
     }
   }
-
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
@@ -19,7 +18,6 @@
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
-
   function _createClass(Constructor, protoProps, staticProps) {
     if (protoProps) _defineProperties(Constructor.prototype, protoProps);
     if (staticProps) _defineProperties(Constructor, staticProps);
@@ -39,7 +37,6 @@
   var _default$2 = /*#__PURE__*/function () {
     function _default(x, y, radius, fn, canvasW, canvasH) {
       _classCallCheck(this, _default);
-
       this.x = x;
       this.y = y;
       this.r = radius;
@@ -47,7 +44,6 @@
       this.canvasW = canvasW;
       this.canvasH = canvasH;
     }
-
     _createClass(_default, [{
       key: "update",
       value: function update() {
@@ -71,17 +67,14 @@
         insideCxt.fillRect(this.x - this.r, this.y - this.r, this.r * 2, this.r * 2);
       }
     }]);
-
     return _default;
   }();
 
   var _default$1 = /*#__PURE__*/function () {
     function _default() {
       _classCallCheck(this, _default);
-
       this.list = [];
     }
-
     _createClass(_default, [{
       key: "update",
       value: function update() {
@@ -112,7 +105,6 @@
         return this.list.length;
       }
     }]);
-
     return _default;
   }();
 
@@ -122,57 +114,52 @@
    * @param totalDistance
    * @returns {number|*|number}
    */
-
   var handleDistanceNum = function handleDistanceNum(num, totalDistance) {
     if (num.toString().indexOf('%') !== -1) {
       return num.split('%')[0] / 100 * totalDistance;
     }
-
     return isString(num) ? Number(num) : num;
   };
+
   /**
    * getTotalDistance
    * @param el
    * @param tip
    * @returns {number|DOMRect}
    */
-
   var getTotalDistance = function getTotalDistance(_ref) {
     var el = _ref.el,
-        _ref$tip = _ref.tip,
-        tip = _ref$tip === void 0 ? 'horizontal' : _ref$tip;
-
+      _ref$tip = _ref.tip,
+      tip = _ref$tip === void 0 ? 'horizontal' : _ref$tip;
     switch (tip) {
       case 'horizontal':
         return el.getBoundingClientRect().width;
-
       case 'vertical':
         return el.getBoundingClientRect().height;
-
       default:
         return el.getBoundingClientRect();
     }
   };
+
   /**
    * getVerticalDistance
    * @param el
    * @param num
    * @returns {number|*}
    */
-
   var getVerticalDistance = function getVerticalDistance(el, num) {
     return handleDistanceNum(num, getTotalDistance({
       el: el,
       tip: 'vertical'
     }));
   };
+
   /**
    * getHorizontalDistance
    * @param el
    * @param num
    * @returns {number|*}
    */
-
   var getHorizontalDistance = function getHorizontalDistance(el, num) {
     return handleDistanceNum(num, getTotalDistance({
       el: el,
@@ -183,14 +170,12 @@
   var _default = /*#__PURE__*/function () {
     function _default(_ref) {
       var context = _ref.context,
-          _ref$width = _ref.width,
-          width = _ref$width === void 0 ? '100%' : _ref$width,
-          _ref$height = _ref.height,
-          height = _ref$height === void 0 ? '100%' : _ref$height,
-          cell = _ref.cell;
-
+        _ref$width = _ref.width,
+        width = _ref$width === void 0 ? '100%' : _ref$width,
+        _ref$height = _ref.height,
+        height = _ref$height === void 0 ? '100%' : _ref$height,
+        cell = _ref.cell;
       _classCallCheck(this, _default);
-
       this.context = isString(context) ? document.querySelector(context) : context;
       this.width = getHorizontalDistance(this.context, width);
       this.height = getVerticalDistance(this.context, height);
@@ -201,20 +186,16 @@
       this.cxt = null;
       this.interval = null;
     }
-
     _createClass(_default, [{
       key: "init",
       value: function init() {
         if (!this.canvas) {
           this._initCanvas();
         }
-
         if (!this.snowList) {
           this.snowList = new _default$1();
-
           this._newSnow();
         }
-
         return this;
       }
     }, {
@@ -226,32 +207,28 @@
         this.canvas.width = this.width;
         this.cxt = this.canvas.getContext('2d');
       }
+
       /**
        * animation start
        */
-
     }, {
       key: "start",
       value: function start() {
         var _this = this;
-
         if (!this.snowList) {
           this.init();
         }
-
         this.state = 'play';
         this.interval = setInterval(function () {
           _this.cxt.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
-
           _this.snowList.update();
-
           _this.snowList.draw(_this.cxt);
         }, 13);
       }
+
       /**
        * animation stop
        */
-
     }, {
       key: "stop",
       value: function stop() {
@@ -260,10 +237,10 @@
         this.interval = null;
         this.cxt.clearRect(0, 0, this.canvas.width, this.canvas.height);
       }
+
       /**
        * clear
        */
-
     }, {
       key: "clear",
       value: function clear() {
@@ -275,15 +252,10 @@
       value: function _newSnow() {
         for (var i = 0; i < this.cell; i += 1) {
           var randomX = this._getRandom('x');
-
           var randomY = this._getRandom('y');
-
           var randomR = this._getRandom('r');
-
           var randomFnx = this._getRandom('fnx');
-
           var randomFny = this._getRandom('fny');
-
           var snow = new _default$2(randomX, randomY, randomR, {
             x: randomFnx,
             y: randomFny
@@ -292,54 +264,43 @@
           this.snowList.push(snow);
         }
       }
+
       /**
        * Generate random x-pos, y-pos or fn functions
        * @param  {string} option x|y|fnx|fny
        * @return {int|Function}
        */
-
     }, {
       key: "_getRandom",
       value: function _getRandom(option) {
         var ret;
         var random;
-
         switch (option) {
           case 'x':
             ret = Math.random() * this.width;
             break;
-
           case 'y':
             ret = (Math.random() - 1) * this.height;
             break;
-
           case 'r':
             ret = 2 + Math.random() * 4;
             break;
-
           case 'fnx':
             random = 27 + Math.random() * 100;
-
             ret = function ret(x, y) {
               return x + 0.5 * Math.sin(y / random);
             };
-
             break;
-
           case 'fny':
             random = 0.4 + Math.random() * 1.4;
-
             ret = function ret(x, y) {
               return y + random;
             };
-
             break;
         }
-
         return ret;
       }
     }]);
-
     return _default;
   }();
 
